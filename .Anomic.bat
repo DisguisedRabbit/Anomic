3d34b5d210563d1417c8e8cb019098b0810e69b6
DisguisedRabbit <78755156+DisguisedRabbit@users.noreply.github.com>
2022-06-19 09:23:07 -0400

    Update .Anomic.bat

--- .Anomic.bat
+++ .Anomic.bat
@@ -883,7 +883,7 @@ coroutine.wrap(function()
         end
     end
 end)()
-telesection1:addKeybind("Click TP Keybind", Enum.KeyCode.Q, function()
+teleSection1:addKeybind("Click TP Keybind", Enum.KeyCode.Q, function()
     if mouse.Target then 
         if currentVehicle ~= nil then
             currentVehicle:SetPrimaryPartCFrame(CFrame.new(mouse.Hit.x, mouse.Hit.y + 5, mouse.Hit.z) * CFrame.new(0,-2,0))
