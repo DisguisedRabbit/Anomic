43342df157a2b3b8eeeb72fe06f0b09bdfc7cf9e
DisguisedRabbit <78755156+DisguisedRabbit@users.noreply.github.com>
2022-06-20 14:45:56 -0400

    Update .Anomic.bat

--- .Anomic.bat
+++ .Anomic.bat
@@ -1018,8 +1018,10 @@ BuySectionAmmo:addSlider("Ammo Amount", 1, 0, 10000, function(v)
     buyAmmoAmount = v
 end)
 BuySectionAmmo:addButton("Buy ammo", function()
-    for i = 1, buyAmmoAmount, 5 do
-        wait(0.01)
+    for i = 1, buyAmmoAmount, 1 do
+        wait(0.1)
+        game:GetService("ReplicatedStorage"):FindFirstChild("_CS.Events").PurchaseTeamItem:FireServer(ammoType,"Single",nil)
+        game:GetService("ReplicatedStorage"):FindFirstChild("_CS.Events").PurchaseTeamItem:FireServer(ammoType,"Single",nil)
         game:GetService("ReplicatedStorage"):FindFirstChild("_CS.Events").PurchaseTeamItem:FireServer(ammoType,"Single",nil)
     end
 end)
